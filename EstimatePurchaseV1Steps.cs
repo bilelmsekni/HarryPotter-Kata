71eea27bd3235b7751e717d7fdd804e6db875456
SkiBLE <msekni.bilel@gmail.com>
2016-04-06 08:18:08 +0200

    DEV | Hello Kata

---
+++ EstimatePurchaseV1Steps.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using NUnit.Framework;
+using TechTalk.SpecFlow;
+
+namespace HarryPotter
+{
+    [Binding]
+    public class EstimatePurchaseV1Steps
+    {
+        private Book _book;
+
+        [Given(@"A book")]
+        public void GivenABook()
+        {
+            _book = new Book {Price = 8};
+        }
+        
+        [Then(@"its price is (.*) €")]
+        public void ThenItsPriceIs(int p0)
+        {
+            Assert.AreEqual(_book.Price, 8);
+        }
+    }
+}
